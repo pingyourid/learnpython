3269e553b72b73f415903ea89d2a2a5df9d49dac
zhangbin31 <zhangbin31@baidu.com>
2021-03-05 14:07:31 +0800

    增加添加topic和添加entity能力

--- practice/18/learning_log/learning_logs/urls.py
+++ practice/18/learning_log/learning_logs/urls.py
@@ -10,5 +10,11 @@ urlpatterns = [
     # 显示所有主题
     path('topics/', views.topics, name='topics'),
     # 特定主体
-    path('topics/<int:topic_id>', views.topic, name='topic')
+    path('topics/<int:topic_id>', views.topic, name='topic'),
+    # 添加新主题
+    path('new_topic/', views.new_topic, name='new_topic'),
+    # 添加新条目
+    path('new_entry/<int:topic_id>', views.new_entry, name='new_entry'),
+    # 编辑条目
+    # path('edit_entry/<int:edit_id>', views.edit_entry, name='edit_entry'),
 ]
--- practice/18/learning_log/learning_logs/views.py
+++ practice/18/learning_log/learning_logs/views.py
@@ -1,5 +1,6 @@
-from django.shortcuts import render
-from .models import Topic
+from django.shortcuts import render, redirect
+from .models import Topic, Entry
+from .forms import TopicForm, EntryForm
 
 def index(request):
     '''学习笔记主页'''
@@ -15,3 +16,30 @@ def topic(request, topic_id):
     entries = topic.entry_set.order_by('-date_added')
     context = {'topic' : topic, 'entries' : entries}
     return render(request, 'learning_logs/topic.html', context)
+
+def new_topic(request):
+    if request.method != 'POST':
+        form = TopicForm()
+    else:
+        form = TopicForm(data=request.POST)
+        if form.is_valid():
+            form.save()
+            return redirect('learning_logs:topics')
+    context = {'form' : form}
+    return render(request, 'learning_logs/new_topic.html', context)
+
+def new_entry(request, topic_id):
+    '''特定主题下添加新条目'''
+    topic = Topic.objects.get(id=topic_id)
+
+    if request.method != 'POST':
+        form = EntryForm()
+    else:
+        form = EntryForm(data=request.POST)
+        if form.is_valid():
+            new_entry = form.save(commit=False)
+            new_entry.topic = topic
+            new_entry.save()
+            return redirect('learning_logs:topic',topic_id=topic_id)
+    context = {'topic':topic, 'form' : form}
+    return render(request, 'learning_logs/new_entry.html', context)
